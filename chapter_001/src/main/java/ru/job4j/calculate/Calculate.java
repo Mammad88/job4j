e6ad755928885bfba5dba1b4a52916a500b5fa05
Mammad88 <bruki_mammad@mail.ru>
2019-06-19 19:27:26 +0300

    1.3. JavaDoc. Code convention[#149213]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+/**
+* Calculate - class name
+* @author Bruki Mammad (bruki_mammad@mail.ru)
+*/
+public class Calculate {
+	/**
+	* Main.
+	* @param args - args.
+	*/
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+	}	
+	
+}	    
+		
+	
