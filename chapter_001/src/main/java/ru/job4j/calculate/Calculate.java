8f79d4fb07f7afec7e09dce44daf04384122286a
Mammad88 <bruki_mammad@mail.ru>
2019-06-25 00:42:22 +0300

    2.4. Использование GIT в IDEA [#149162]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,19 +1,19 @@
 package ru.job4j.calculate;
 
 /**
-* Calculate.
-* @author Bruki Mammad (bruki_mammad@mail.ru)
-* @version 2.0
-* @since 0.1 
-*/
+ * Calculate.
+ * @author Bruki Mammad (bruki_mammad@mail.ru)
+ * @version 2.0
+ * @since 0.1
+ */
 public class Calculate {
 	/**
 	 * Main method.
 	 * @param args - args.
 	 */
 	public static void main(String[] args) {
-	    System.out.println("Hello World");
-	}	
+		System.out.println("Hello World");
+	}
 	/**
 	 * Method echo.
 	 * @param name Bruki Mammad.
