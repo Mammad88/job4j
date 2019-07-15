6069381a14dc877b159fc4ea8e60ac06d50fcf0a
Mammad88 <bruki_mammad@mail.ru>
2019-07-15 19:33:42 +0300

    "5.2. Создать программу, вычисляющую факториал.[#149181]"

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -7,7 +7,7 @@ package ru.job4j.condition;
  */
 public class MultiMax {
     public int max(int first, int second, int third) {
-        /**
+       /**
          * Method - max.
          */
         int result = first > second ? first : second;
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+       Factorial fact = new Factorial();
+       int result = fact.calc(5);
+       int expected = 120;
+        assertThat(result, closeTo(expected, 120));
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        Factorial fact = new  Factorial();
+        int result = fact.calc(0);
+        int expected = 1;
+        assertThat(result, closeTo(expected, 1));
+    }
+
+    private void assertThat(int result, Matcher<Double> closeTo) {
+    }
+
+}
+
