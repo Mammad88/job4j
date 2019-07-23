7a91245680ac0516bc629a03673b75adcc14de4e
Mammad88 <bruki_mammad@mail.ru>
2019-07-23 11:03:03 +0300

    "программа по расходу топлива в автомобиле  Audi."

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Car.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+/**
+ * Класс Car рассчитывает сколько машина может проехать километрров.
+ */
+public class Car {
+    private double volume;
+// поле private пишется внутри класса.
+    public void drive(int kilometer) {
+        this.volume = this.volume - kilometer;
+        // метод drive рассчитывает сколько километров машина проедет.
+
+    }
+    public void fill(int gas) {
+        this.volume = this.volume + 10 * gas;
+        // метод fill -  рассчитывае сколько топлива надо машине.
+
+
+
+    }
+    public boolean canDrive() {
+        boolean result = this.volume > 0;
+        return result;
+    }
+    public void gasInfo() {
+        System.out.println("I can drive " + this.volume + " kilometers");
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculator/Size.java
@@ -0,0 +1,40 @@
+package ru.job4j.calculator;
+
+/**
+ * class Size.
+ */
+public class Size {
+    /**
+     * Метод add складывает left и right.
+     * @param left
+     * @param right
+     * @return result.
+     */
+    public int add(int left, int right) {
+    int result = left + right;
+    return result;
+    }
+
+    /**
+     * Метод info - выводить информацию на консоль.
+     * @param size
+     */
+    public void info(int size) {
+    System.out.println("Your size is: " + size);
+    }
+
+    /**
+     * метод main.
+     * @param args
+     */
+    public static void main(String[] args) {
+    Size size = new Size();
+    int nike = 10;
+    int puma = 50;
+    int value = size.add(nike, puma);
+    // складывает методом add, nike и puma.
+    size.info(value);
+    }
+}
+
+
