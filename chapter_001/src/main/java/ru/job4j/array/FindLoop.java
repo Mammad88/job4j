38bbdd3cf01b1e7e421c18491543995b0680139e
Mammad88 <bruki_mammad@mail.ru>
2019-07-27 11:24:34 +0300

    "6.1. Классический поиск перебором.[#149187]"

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * @author Bruki Mammad.
+ * @version $id$
+ * @since 27.07.2019
+ */
+public class FindLoop {
+    /**
+     * class - FindLoop.
+     * Найти индекс элемента, удовлетворяющий условию.
+     * @param data - список элементов.
+     * @param el - индекс, который нужно найти.
+     * @return rst - индекс элемента удовлетворяющий условию.
+     */
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = 0; index < el; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
