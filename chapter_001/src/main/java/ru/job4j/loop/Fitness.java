2829913ef3f4faf6e889faddef64a56475073b6d
Mammad88 <bruki_mammad@mail.ru>
2019-07-28 19:21:49 +0300

    "5.4. Протеиновая диета. [#156313]"

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+/**
+ * Fitness.
+ * @author Bruki Mammad (bruki_mammad@mail.ru)
+ * @version $id$
+ * @since 28.07.2019
+ */
+public class Fitness {
+    /**
+     *
+     * @param ivan -  увеличивает  силу тяги в 3 раза.
+     * @param nik - увеличивает силу тяги в 2 раза.
+     * @return month.
+     */
+
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+        return month;
+    }
+}
