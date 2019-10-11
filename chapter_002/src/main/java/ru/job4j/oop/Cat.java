37ce4aebaa018e9c4c0174d394c3de74c98ac0f0
Mammad88 <bruki_mammad@mail.ru>
2019-10-11 07:11:56 +0300

    "1.1. Создание объекта [#165452]"

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+/**
+ * @author Bruki Mammad (bruki_mammad@mail.ru)
+ * @version $id$
+ * @since 10.10.2019
+ * class - Cat.
+ * Создание объекта типа Cat.
+ */
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+/**
+ * @author Bruki Mammad (bruki_mammad@mail.ru)
+ * @version $id$
+ * @since 10.10.2019
+ * class - Dog.
+ * Создание объектов типа Dog.
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
