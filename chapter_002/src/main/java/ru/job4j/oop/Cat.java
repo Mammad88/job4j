3deccdfce42d9da72a445c06f19b0f446a7be6d0
Mammad88 <bruki_mammad@mail.ru>
2019-10-17 22:26:09 +0300

    "1.5. Поля объекта [#166083]"

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -3,34 +3,47 @@ package ru.job4j.oop;
 /**
  * @author Bruki Mammad (bruki_mammad@mail.ru)
  * @version $id$
- * @since 10.10.2019
+ * @since 17.10.2019
  * class - Cat.
- * Создание объекта типа Cat.
+ * Создание поля String name.
  */
 public class Cat {
     private String name;
 
+    /**
+     * method show - показывает имя котика и что он ел.
+     */
     public void show() {
         System.out.println("ел " + this.name);
     }
 
+    /**
+     * method eat.
+     * @param meat - мясо.
+     */
     public void eat(String meat) {
         this.name = meat;
-           }
+    }
 
+    /**
+     * method  giveNick - хранит nick Cat.
+     * @param nick - nick.
+     */
     public void giveNick(String nick) {
         this.name = nick;
         System.out.println(nick);
     }
     public static void main(String[] args) {
-
-
+        /**
+         * Создание объекта gav типа Cat.
+         */
         Cat gav = new Cat();
-
         gav.giveNick("Tom");
-        gav.eat("cutlet ");
+        gav.eat("cutlets ");
+        /**
+         * Создание объекта black типа Cat.
+         */
         gav.show();
-
         Cat black = new Cat();
         gav.giveNick("Joni");
         black.eat("fish");
