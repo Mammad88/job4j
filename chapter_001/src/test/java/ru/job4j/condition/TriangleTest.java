b5494e15d2c9243160bee1374f7869216587d382
Mammad88 <bruki_mammad@mail.ru>
2019-10-19 21:33:25 +0300

    "2.2. Рефакторинг - Площадь треугольника.[#149286]"

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,24 +1,31 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-
 import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
 
 /**
  * class TriangleTest.
- * Создаем объект треугольник.
- * вычисляем площадь треугольника.
- * Проверяем результат и ожидаемое значение.
  */
 
 public class TriangleTest {
-    @Test
-    public void whenAreaSetThreePointsThenTriangleArea() {
-        Triangle triangle = new Triangle();
-        double result = triangle.area(0, 0, 0, 2, 2, 0);
-        double expected = 2D;
-        assertThat(result, closeTo(expected, 0.1));
-    }
+    /**
+     * Test программы по вычислению площади треугольника.
+     */
+      @Test
+      public void whenAreaSetThreePointsThenTriangleArea() {
+          // Создаем три объекта класса Point.
+          Point first = new Point(0, 0);
+          Point second = new Point(0, 2);
+          Point third = new Point(2, 0);
+          // Создаем объект треугольник и передаем в него объекты точек.
+          Triangle triangle = new Triangle(first, second, third);
+          // Вычисляем площадь треугольника.
+          double result = triangle.area();
+          // Задаем ожидаемый результат.
+          double expected = 2D;
+          // Проверяем результат и ожидаемое значение.
+          System.out.println(String.format("Result is %s", result));
+          assertThat(result, closeTo(expected, 0.1));
+      }
 }
-
