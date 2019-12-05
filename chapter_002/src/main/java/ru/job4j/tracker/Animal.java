ec0517f79b581ecb92f631eef62ceaf14cae8b9a
Mammad88 <bruki_mammad@mail.ru>
2019-12-05 22:07:30 +0300

    "1. Конструктор по умолчанию. [#173889] "

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class Animal {
+
+    public static void main(String[] args) {
+        Predator predator = new Predator();
+        Tiger tiger = new Tiger();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class Tiger extends Animal {
+
+    public Tiger() {
+        super();
+        System.out.println("Tiger!!!");
+    }
+}
