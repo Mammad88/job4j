280402186cc163663d0a6260a190e3a77958af81
Mammad88 <bruki_mammad@mail.ru>
2019-12-13 09:15:53 +0300

    "5. Исправление класса JSONReport в  Аннотации [#174891]"

--- chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -3,8 +3,8 @@ package ru.job4j.inheritance;
 public class ReportUsage {
 
     public static void main(String[] args) {
-        HtmlReport htmlReport = new HtmlReport();
-        String text = htmlReport.generate("name ", " body");
+        JSONReport jsonReport = new JSONReport();
+        String text = jsonReport.generate("name", "body");
         System.out.println(text);
     }
 }
