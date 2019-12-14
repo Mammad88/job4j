bdcc58478ec4da1bdca1bd9112437bafbe1f76c1
Mammad88 <bruki_mammad@mail.ru>
2019-12-14 21:20:52 +0300

    "5. Реализовать класс Tracker [#396]"

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,8 +1,85 @@
 package ru.job4j.tracker;
 
-
+import java.util.Random;
 
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
 
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище.
+     * @param item новая заявка.
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    /**
+     * Метод findById выполняет поиск заявок по id.
+     * @param id.
+     * @return result.
+     */
+    public Item findById(String id) {
+        Item result = null;
+        for (Item item: items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод выводит список всех заявок.
+     * @return result.
+     */
+
+    public Item[] findAll() {
+        Item[] result = new Item[position];
+        for (int index = 0; index != this.position; index++) {
+            result[index] = this.items[index];
+        }
+        return result;
+    }
+
+    /**
+     * Поиск заявок по наименованию и копирование их в массив.
+     * @param key -  ввод имени для поиска.
+     * @return список заявок.
+     */
+    public Item[] findByName(String key) {
+        Item[] temp = new Item[position];
+        int count = 0;
+        for (int index = 0; index < position; index++) {
+            if (this.items[index].getName().equals(key)) {
+                temp[count] = this.items[index];
+                count++;
+            }
+        }
+        Item[] listOfNames = new Item[count];
+        for (int index = 0; index < count; index++) {
+            listOfNames[index] = temp[index];
+        }
+        return  listOfNames;
+    }
 }
-
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenFindAllThenReturnAll() {
+        Tracker tracker = new Tracker();
+        // Создаем три заявки и добавляем их все в трекер. Каждая получает уникальный id.
+        Item first = new Item("100L", "test1");
+        tracker.add(first);
+        Item second = new Item("200L", "test2");
+        tracker.add(second);
+        Item third = new Item("300L", "test3");
+        tracker.add(third);
+        // Создаем ожидаемый массив класса Item.
+        Item[] expected = new Item[3];
+        expected[0] = first;
+        expected[1] = second;
+        expected[2] = third;
+        // Проверяем, что массив, передаваемый через метод findAll и ожидаемый массив равны.
+        assertThat(tracker.findAll(), is(expected));
+    }
+
+    @Test
+    public void whenFindByIdThenReturnItem() {
+        Tracker tracker = new Tracker();
+        // Создаем три заявки и добавляем их все в трекер. Каждая получает уникальный id.
+        Item first = new Item("123L",  "test1");
+        tracker.add(first);
+        Item second = new Item("234L",  "test2");
+        tracker.add(second);
+        Item third = new Item("456L", "test3");
+        tracker.add(third);
+        // Создаем объект класса Item и присваеваем ему id одного из элементов добавленных в трекер.
+        Item expected = new Item("234L",  "test2");
+        expected.setId(second.getId());
+        // Сравниваем, что имя возвращенного элемента методом findById равно имени ожидаемого элемента.
+        assertThat(tracker.findById(second.getId()).getName(), is("test2"));
+    }
+
+    @Test
+    public void whenFindByNameThenReturnByName() {
+        Tracker tracker = new Tracker();
+        // Создаем три заявки и добавляем их все в трекер. Каждая получает уникальный id, две имеют одинаковое имя.
+        Item first = new Item("123L", "test1");
+        tracker.add(first);
+        Item second = new Item("234L", "test2");
+        tracker.add(second);
+        Item third = new Item("456L", "test2");
+        tracker.add(third);
+        // Создаем ожидаемый массив класса Item из двух элементов с одинаковым именем.
+        Item[] expected = new Item[2];
+        expected[0] = second;
+        expected[1] = third;
+        // Проверяем, что массив, передаваемый через метод findAll и ожидаемый массив равны.
+        assertThat(tracker.findByName("test2"), is(expected));
+    }
+
+
+}
