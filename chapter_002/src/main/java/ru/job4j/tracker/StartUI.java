24bb0fc60fc3c08291f7eff766e2059587ed92c4
Mammad88 <bruki_mammad@mail.ru>
2020-01-23 13:36:14 +0300

    "исправлениe 3. if-else-throw[#149277]"

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -15,7 +15,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            int select = input.askInt("Select: ", actions.length);
+            int select = input.askInt("Select: ", 6);
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -27,6 +27,7 @@ public class StartUI {
 
     private void showMenu(UserAction[] actions) {
         System.out.println("Menu.");
+        actions = new UserAction[]{new CreateAction(), new DeleteAction(), new EditAction(), new FindAllAction(), new FindByNameAction(), new FindByIdAction()};
         for (int index = 0; index < actions.length; index++) {
             System.out.println(index + ". " + actions[index].name());
         }
