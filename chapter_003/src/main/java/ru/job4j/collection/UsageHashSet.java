13bca21f8b2aa12452f1956e7b10efadfdccc68e
Mammad88 <bruki_mammad@mail.ru>
2020-03-03 07:10:56 +0300

    "0. Колекциия HashSet. Обобшения[#149246]"

---
+++ chapter_003/src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+/**
+ * class UsageHashSet использование HashSet.
+ *
+ * @author Bruki Mammad (bruki_mammd@mail.ru)
+ * @version $1.0$
+ * @since 02.03.2020
+ */
+public class UsageHashSet {
+    public static void main(String[] args) {
+        /**
+         * вставка элементов в колекцию HashSet через метод add.
+         */
+        HashSet<String> autos = new HashSet<String>();
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        // чтение даннаых из массива.
+        for (String auto : autos) {
+            System.out.println(auto);
+        }
+    }
+}
