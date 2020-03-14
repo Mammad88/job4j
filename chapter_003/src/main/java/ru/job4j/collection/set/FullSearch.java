055123532c3e339278498ce998739ac020f354a8
Mammad88 <bruki_mammad@mail.ru>
2020-03-14 13:30:41 +0300

    " ver. 3.0. - 1. Уникальные задачи. [#212653]"

--- chapter_003/src/main/java/ru/job4j/collection/set/FullSearch.java
+++ chapter_003/src/main/java/ru/job4j/collection/set/FullSearch.java
@@ -3,13 +3,25 @@ package ru.job4j.collection.set;
 import java.util.HashSet;
 import java.util.List;
 
+/**
+ * Уникальные задачи.
+ *
+ * @author Bruki Mammad (bruki_mammad@mail.ru)
+ * @version 3.0
+ * @since 10.03.2020
+ */
 public class FullSearch {
+
+    /**
+     * Method extractNumber -  убирает дубликаты из списка, полученного от системы.
+     *
+     * @param list - коллекцию задач с номерами и описанием.
+     * @return уникальные номера задач.
+     */
     public static HashSet<String> extractNumber(List<Task> list) {
         HashSet<String> numbers = new HashSet<>();
         for (Task task : list) {
-            if (numbers.isEmpty() && list.indexOf(task) == list.lastIndexOf(task)) {
-                numbers.remove(list.lastIndexOf(task));
-            }
+            numbers.add(task.getNumber());
         }
         return numbers;
     }
