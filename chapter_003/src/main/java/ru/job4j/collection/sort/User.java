050af7bcadd47aa3d6fa47d36237675297cd794c
Mammad88 <bruki_mammad@mail.ru>
2020-03-24 20:55:46 +0300

    "1. Организовать сортировку User [#10034]"

---
+++ chapter_003/src/main/java/ru/job4j/collection/sort/User.java
@@ -0,0 +1,53 @@
+package ru.job4j.collection.sort;
+
+import java.util.Objects;
+
+/**
+ * class User - пользователь.
+ *
+ * @author Bruki Mammad (bruki_mammad@mail.ru)
+ * @version $1.0$
+ * @since 22.03.2020
+ */
+public class User implements Comparable<User> {
+    /**
+     * Имя пользователя.
+     */
+    private String name;
+    /**
+     * Возраст пользователя.
+     */
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User user) {
+        return name.compareTo(user.name) == 0 ? (age - user.age) : name.compareTo(user.name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
+    }
+}
